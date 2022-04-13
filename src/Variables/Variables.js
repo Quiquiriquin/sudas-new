aafe4cedaa1a1579bc3eae32a52f5dada9b5e1ce
Quiquiriquin <enrique.gintelligence@gmail.com>
2022-04-13 00:29:31 -0500

    hange domain

--- src/Variables/Variables.js
+++ src/Variables/Variables.js
@@ -1,2 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
-export const API = 'https://enigmatic-inlet-56197.herokuapp.com/api/v1';
+export const API =
+  'https://enigmatic-inlet-56197.herokuapp.com/api/v1';
