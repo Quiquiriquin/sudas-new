deec7bdbcf1ff332de2156e6a11c39688eb0b7ce
Quiquiriquin <enrique.gintelligence@gmail.com>
2022-04-25 17:38:55 -0500

    Chanhes

--- src/Variables/Variables.js
+++ src/Variables/Variables.js
@@ -1,3 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export const API =
   'https://enigmatic-inlet-56197.herokuapp.com/api/v1';
+// export const API = 'http://localhost:4040/api/v1';
